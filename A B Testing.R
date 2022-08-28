fe48adc2ddf7e7941ca4fb595b584cf242865cad
Hannah Tabea Roos <hannahtabea.roos@gmail.com>
2022-08-28 16:08:19 +0200

    Preliminary analysis
    Freq: Run power analysis, t-test changed and boxplots
    Bayesian: changed prior parameters, A/B Test as well as posterior visualizations

--- A B Testing.R
+++ A B Testing.R
@@ -29,72 +29,109 @@ facebook_dat %>%
   group_by(Type) %>%
   summarise(Reach = sum(`Lifetime Post Total Reach`))
 
-# F: power analysis for testing videos against photos
-# given that we would downsample photo group
-library(powerMediation)
-total_sample_size <- SSizeLogisticBin (p1 = 0.01,
-                                       p2 = 0.06,
-                                       B = 0.15,
-                                       alpha = 0.05,
-                                       power = 0.8)
-total_sample_size 
+#-----------------------------------------------------------
+# Experimental Design and planning
+#------------------------------------------------------------
 
-total_sample_size / 2
-
-# which power do we get with a setup like ours?
+# F: power analysis for testing videos against status
+# Load package to run power analysis
 library(pwr)
 
-# unequal sample size
-n1 = 45
-n2 = 7
-p1 = 0.01
-p2 = 0.06
-h = abs(2*asin(sqrt(p1))-2*asin(sqrt(p2)))
+# Run power analysis for t-test
+sample_size <- pwr.t.test(d = 1,
+                          sig.level = 0.05,
+                          power = 0.8,
+                          alternative = "greater")
+sample_size
 
-pwr.2p2n.test(h, n1=n1, n2=n2, sig.level=0.10)
-
-
-
+#------------------------------------------------------------
 # B: prior specification (read paper)
 library("abtest")
-prior_par <- elicit_prior(q = c(2, 6, 8),
+prior_par <- elicit_prior(q = c(0.02, 0.05, 0.08),
                              prob = c(.025, .5, .975),
                              what = "arisk")
+plot_prior(prior_par, what = "arisk")
 plot_prior(prior_par, what = "logor")
-plot_prior(prior_par, what = "p1p2")
 
-# F: Plot Frequencies A vs B success and run logistic regression
+#------------------------------------------------------------
+# Hypothesis Testing
+#------------------------------------------------------------
+# F: run t-test
+ab_experiment_results <- t.test(ER ~ Type,
+                                data = facebook_dat_ER,
+                                alternative = "less")
+ab_experiment_results
 
-
-
-# B: Posterior success probabilities
+#------------------------------------------------------------
+# B: Bayes Factor
 set.seed(1)
 
 # how many engaged users/reach per type? - number of success
-success_video <- facebook_dat %>% 
-                  filter(Type == 'Video') %>%
-                  select(`Lifetime Engaged Users`)
-success_status <- facebook_dat %>% 
-                    filter(Type == 'Status') %>%
-                    select(`Lifetime Engaged Users`)
+facebook_dat_ER %>% 
+                  group_by(Type) %>%
+                  summarise(Engagement = sum(`Lifetime Engaged Users`))
 
 # how many reached users per type? - number of trials
-trials_video <- facebook_dat %>% 
-                  filter(Type=='Video') %>%
-                  select(`Lifetime Post Total Reach`)
-trials_status <- facebook_dat %>% 
-                  filter(Type=='Status') %>%
-                  select(`Lifetime Post Total Reach`)
+facebook_dat_ER %>% 
+                  group_by(Type) %>%
+                  summarise(Reach = sum(`Lifetime Post Total Reach`))
 
-data <- list(y1 = success_status, n1 = trials_status, y2 = success_video, n2 = trials_video)
+
+data <- list(y1 = 91810, n1 = 588550, y2 = 11949, n2 = 358440)
 ab <- ab_test(data = data, prior_par = prior_par)
 print(ab)
 
+#plot posterior
+png(filename="posterior_p1p2.png", width= 800, height= 550)
+plot_posterior(ab, what = "p1p2")
+dev.off()
+
+png(filename="posterior_risk.png", width= 800, height= 550)
+plot_posterior(ab, what = "arisk")
+dev.off()
+
+
+#------------------------------------------------------------
+# Magnitude of evidence
+#------------------------------------------------------------
+# F: eyeball difference with boxplot
+facebook_dat_ER <- facebook_dat %>% 
+  filter(Type == 'Video' | Type == 'Status') %>%
+  group_by(Type) %>%
+  mutate(ER = `Lifetime Engaged Users`/`Lifetime Post Total Reach`,)
+
 # F: Boxplot of user engagement A vs B
+library(ggplot2)
+
+png(filename="boxplot.png", width= 800, height= 550)
+ggplot(facebook_dat_ER,
+                  aes(x = Type,
+                      y = ER)) +
+                  geom_boxplot()+
+                  ggtitle("User engagement Rate by Content Type")
+dev.off()
+------------------------------------------------------------
 # B: Magnitude of success with probability wheel
+prob_wheel(ab)
+
+#-------------------------------------------------------------
+# Accumulation of evidence
+#------------------------------------------------------------
 
 # F: sequential analysis
-# B: emergence of evidence 
+library(gsDesign)
+seq_analysis <- gsDesign(k = 3,
+                         test.type = 1,
+                         alpha = 0.05,
+                         beta = 0.2,
+                         sfu = "Pocock")
+# we have a total of 52 observations
+max_n <- 52
+max_n_per_group <- max_n / 2
+stopping_points <- max_n_per_group * seq_analysis$timing
+stopping_points
 
-# F: no way
-# B: robustness of evidence with heatmap
+# B: emergence of evidence 
+png(filename="seq_plot.png", width= 800, height= 550)
+plot_sequential(ab)
+dev.off()
